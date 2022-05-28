955fca649f0ad5117c4346ce3091a3f19d39fe0e
pc4rt <ryantan111100@gmail.com>
2022-05-28 14:33:35 +0800

    changed settings.py to include django_heroku

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,3 +131,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Informs Django to perform authentication using the CustomUser model
 AUTH_USER_MODEL = "stockmarket.CustomUser"
+# default configuration for deployment to Heroku
+django_heroku.settings(locals())
