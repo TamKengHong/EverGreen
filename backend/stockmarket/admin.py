a44a045849ec12a10c720c430682e01049b11482
pc4rt <ryantan111100@gmail.com>
2022-05-30 19:14:13 +0800

    added comment to admin.py

--- backend/stockmarket/admin.py
+++ backend/stockmarket/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import CustomUser,Post
+from .models import CustomUser,Post,Comment
 
 # Register your models here.
 # The register function is used to add models to the Django admin so that CRUD operations can be performed on the data in those models through the user interface.
 admin.site.register(CustomUser)
 admin.site.register(Post)
+admin.site.register(Comment)
