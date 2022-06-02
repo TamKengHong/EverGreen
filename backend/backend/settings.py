b752b7e3423c67314ead58af884ca1586fb46cfc
pc4rt <ryantan111100@gmail.com>
2022-06-02 18:49:22 +0800

    Added TokenAuthentication to REST_FRAMEWORK in settings.py, added filter to views.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
 
--- backend/stockmarket/views.py
+++ backend/stockmarket/views.py
@@ -1,8 +1,13 @@
 from .models import CustomUser,Post,Comment
 from .serializers import CustomUserSerializer
 from rest_framework import viewsets,filters,permissions
+from rest_framework.authentication import TokenAuthentication
 
+#default authentication class for all viewsets is TokenAuthentication, as shown in settings.py
 class CustomUserViewset(viewsets.ModelViewSet):
     serializer_class = CustomUserSerializer
     queryset = CustomUser.objects.all() #retrieve all CustomUsers from database
     permission_classes = [permissions.IsAuthenticated]
+    # Allow users to search/filter other users by their names or email by making queries such as http://example.com/api/users?search=russell
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'email']
