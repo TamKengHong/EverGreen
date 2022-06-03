116889ea4a6b50382111048d1c54f73e16f63251
pc4rt <ryantan111100@gmail.com>
2022-06-03 15:56:13 +0800

    added LoginViewSet to views.py

--- backend/stockmarket/urls.py
+++ backend/stockmarket/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
-from rest_framework.authtoken import views
-from .views import CustomUserViewSet
+from .views import CustomUserViewSet,LoginViewSet
 
 router = routers.DefaultRouter()
 router.register("users",CustomUserViewSet)
-
-urlpatterns = [path('', include(router.urls)),
-    path('api-token-auth/', views.obtain_auth_token)
-]
+#specify basename for LoginViewSet since LoginViewSet does not have queryset attribute
+router.register("auth",LoginViewSet,basename="login")
+urlpatterns = [path('', include(router.urls)),]
