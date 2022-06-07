313c6e18bff9d7ef88e5abdfc7b1362b6d1c11d9
pc4rt <ryantan111100@gmail.com>
2022-06-07 16:07:12 +0800

    Added PostSerializer and CommentSerializer

--- backend/stockmarket/serializers.py
+++ backend/stockmarket/serializers.py
@@ -47,3 +47,13 @@ class CustomRegisterSerializer(RegisterSerializer):
         user = super().save(request)
         user.save()
         return user
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__' 
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__' 
