074de486bb1b6782ad8276378af044feb9d2bc77
pc4rt <ryantan111100@gmail.com>
2022-06-26 20:38:21 +0800

    Created custom search filter for CustomUser,Post,Comment,Bookmark

--- backend/stockmarket/views.py
+++ backend/stockmarket/views.py
@@ -1,9 +1,11 @@
 from .models import CustomUser,Post,Comment,Bookmark
 from .serializers import CustomUserSerializer,PostSerializer,CommentSerializer,BookmarkSerializer
-from rest_framework import viewsets,filters
+from .permissions import CustomUserPermissions,PostPermissions,CommentPermissions,BookmarkPermissions
+from .filters import CustomUserSearchFilter, PostSearchFilter, CommentSearchFilter,BookmarkSearchFilter
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from .permissions import CustomUserPermissions,PostPermissions,CommentPermissions,BookmarkPermissions
+
 
 #default authentication class for all viewsets is TokenAuthentication, as shown in settings.py
 #CustomViewSet is used to get user details
@@ -13,7 +15,7 @@ class CustomUserViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [CustomUserPermissions] #only authenticated users can get user details
     # Allow users to search/filter other users by their usernames or email by making queries such as http://example.com/api/users?search=russell
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [CustomUserSearchFilter]
     search_fields = ['username', 'email']
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -22,7 +24,7 @@ class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [PostPermissions]
     # Allow users to search for a post by the author's username, email, post title, or the associated stock ticker
-    filter_backends = [filters.SearchFilter]
+    filter_backends = [PostSearchFilter]
     search_fields = ['name__username','name__email','title','stockTicker']
 
 class CommentViewSet(viewsets.ModelViewSet):
@@ -31,8 +33,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [CommentPermissions]
     # Allow users to search for a comment by the commenter's name or the post's associated stock ticker
-    filter_backends = [filters.SearchFilter] 
-    search_fields = ['name','post__stockTicker']
+    filter_backends = [CommentSearchFilter] 
+    search_fields = ['name__username','name__email','post__stockTicker']
 
 class BookmarkViewSet(viewsets.ModelViewSet):
     serializer_class = BookmarkSerializer
@@ -40,5 +42,5 @@ class BookmarkViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [BookmarkPermissions]
     # Allow users to search for a comment by the commenter's name or the post's associated stock ticker
-    filter_backends = [filters.SearchFilter] 
-    search_fields = ['name','post__stockTicker']
+    filter_backends = [BookmarkSearchFilter] 
+    search_fields = ['name__username','post__stockTicker']
