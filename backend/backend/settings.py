7dffb7634821afce6e8e160353c9bea674c225a0
pc4rt <ryantan111100@gmail.com>
2022-06-27 14:35:08 +0800

    Changed STATIC_ROOT and MEDIA_ROOT in settings.py, and added STATICFILES_DIRS to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -141,10 +141,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static_folder")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(PROJECT_DIR, "static")
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(PROJECT_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(PROJECT_DIR, "media_folder")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
