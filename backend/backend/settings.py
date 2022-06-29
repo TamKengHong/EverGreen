d585ee48dc6bf6729e6bf3291d9fa62fa4a4694d
pc4rt <ryantan111100@gmail.com>
2022-06-29 16:10:46 +0800

    Changed default database name in settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
