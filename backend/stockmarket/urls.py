21e53d5c8ad0f4a25b853ad50fc384cf2909cd09
pc4rt <ryantan111100@gmail.com>
2022-07-02 18:34:43 +0800

    fixed import bug in urls.py

--- backend/stockmarket/urls.py
+++ backend/stockmarket/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from .views import CustomUserViewSet,PostViewSet,CommentViewSet,BookmarkViewSet
-import redditpraw
+from .redditpraw import script_runner
 
 router = routers.DefaultRouter()
 router.register("users",CustomUserViewSet)
 router.register("posts",PostViewSet)
 router.register("comments",CommentViewSet)
 router.register("bookmarks",BookmarkViewSet)
-urlpatterns = [path('', include(router.urls)),path('reddit/',redditpraw.script_runner)]
+urlpatterns = [path('', include(router.urls)),path('reddit/',script_runner)]
