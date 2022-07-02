1292952993a3a85ed6e7a3c9538f849e3899af38
pc4rt <ryantan111100@gmail.com>
2022-07-02 18:32:35 +0800

    added URL to run reddit scraping script in stockmarket.urls.py

--- backend/stockmarket/urls.py
+++ backend/stockmarket/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from .views import CustomUserViewSet,PostViewSet,CommentViewSet,BookmarkViewSet
+import redditpraw
 
 router = routers.DefaultRouter()
 router.register("users",CustomUserViewSet)
 router.register("posts",PostViewSet)
 router.register("comments",CommentViewSet)
 router.register("bookmarks",BookmarkViewSet)
-urlpatterns = [path('', include(router.urls)),]
+urlpatterns = [path('', include(router.urls)),path('reddit/',redditpraw.script_runner)]
