860eb7b84c2160b4d5263a8ac68484dfc7525f76
Tam Keng Hong <tamkenghong@gmail.com>
2022-07-19 00:26:14 +0800

    Added FullCalendar, added routing to events, added Display all Events, Added month/week/day/list view to calendar

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,14 +1,16 @@
-import { Text } from '@chakra-ui/react'
-import SearchBar from '../components/SearchBar'
+import { Text, Box } from '@chakra-ui/react'
+import UserCalendar from '../components/User/UserCalendar'
 
 const Test = () => {
   return (
-    <>
+    <Box bg="gray.300">
       <Text fontSize="60">
         Test
       </Text>
-      <SearchBar />
-    </>
+      <Box w="90%" margin="auto" bg="gray.50">
+        <UserCalendar />
+      </Box>
+    </Box >
   )
 }
 
