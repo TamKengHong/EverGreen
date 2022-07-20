804c4e602dd7741eee67fa468d1d63e614f6076d
pc4rt <ryantan111100@gmail.com>
2022-07-20 16:42:28 +0800

    Created endpoint /earningsmove to get earnings move for a particular stock

--- backend/stockmarket/earningsmove.py
+++ backend/stockmarket/earningsmove.py
@@ -1,4 +1,10 @@
+from multiprocessing import allow_connection_pickling
 import yfinance as yf
+from django.http import QueryDict
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
 
 def calculate_implied_directional_move(current_stock_price,directional_options):
     #use 1 billion as an arbitrarily large number
@@ -21,3 +27,11 @@ def calculate_implied_move(STOCK):
     implied_calls_move = calculate_implied_directional_move(current_stock_price,optchain.calls)
     implied_puts_move = calculate_implied_directional_move(current_stock_price,optchain.puts)
     return round((implied_calls_move + implied_puts_move),2)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def script_runner(request):
+    data = QueryDict(request.body)
+    STOCK = data.get("stock")
+    data = {"ticker": STOCK,"implied_earnings_move": calculate_implied_move(STOCK)}
+    return Response(data)
--- backend/stockmarket/urls.py
+++ backend/stockmarket/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
 from .views import CustomUserViewSet,PostViewSet,CommentViewSet,BookmarkViewSet,ScrapingModelViewSet,UpcomingEarningsViewSet
+from .earningsmove import script_runner
 
 router = routers.DefaultRouter()
 router.register("users",CustomUserViewSet)
@@ -10,4 +11,4 @@ router.register("comments",CommentViewSet)
 router.register("bookmarks",BookmarkViewSet)
 router.register("scrape",ScrapingModelViewSet)
 router.register("earnings",UpcomingEarningsViewSet)
-urlpatterns = [path('', include(router.urls))]
+urlpatterns = [path('', include(router.urls)),path('earningsmove/',script_runner)]
