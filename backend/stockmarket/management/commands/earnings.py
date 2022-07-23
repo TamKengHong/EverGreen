803dd66c25a13b24c1431d6292e21244d288a9cc
pc4rt <ryantan111100@gmail.com>
2022-07-23 16:48:03 +0800

    Added functions in earnings.py to check if earnings call takes place before market hours or after market hours

--- backend/stockmarket/management/commands/earnings.py
+++ backend/stockmarket/management/commands/earnings.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from stockmarket.models import UpcomingEarnings
 import requests,csv,datetime,pickle,os
 from dateutil.relativedelta import relativedelta
+import yfinance as yf
 
 #default period is 3 months
 def earnings_calendar(AV_KEY,PERIOD="3month",STOCK=None):
@@ -36,16 +37,36 @@ def filter_for_earnings_of_stocks_above_2B(earnings_list):
     with open(file_path,"rb") as f:
         stocks_above_2B= pickle.load(f)
     return list(filter(lambda row: row[0] in stocks_above_2B,earnings_list))
-    
+
+#returns a Python Datetime object in UTC
+def get_upcoming_earnings_date(STOCK):
+    stock = yf.Ticker(STOCK)
+    upcoming_earnings_date = stock.calendar.loc["Earnings Date"]["Value"]
+    #converts the Pandas Timestamp object into a native Python datetime object
+    return upcoming_earnings_date.to_pydatetime()
+
+#checks if upcoming_earnings_date is premarket or afterhours
+def classify_upcoming_earnings_date(upcoming_earnings_date):
+    #convert UTC to eastern time by subtracting four hours, since the given time is in UTC
+    eastern_time = (upcoming_earnings_date - datetime.timedelta(hours=4)).time()
+    #the US stock market opens at 9:30am and closes at 4pm
+    OPENING_TIME,CLOSING_TIME = datetime.time(9,30),datetime.time(16,0)
+    if eastern_time <= OPENING_TIME:
+        return "Before market open"
+    if eastern_time >= CLOSING_TIME:
+        return "After market close"
+    #return an error message if the time is during official stock market hours
+    return "An error has occurred; the time coincides with operation hours"
+
 #consolidate earnings by dates
 def process(sorted_earnings_list):
     dct = {}
     for row in sorted_earnings_list:
         ticker,date = row[0],row[2]
+        classification = classify_upcoming_earnings_date(get_upcoming_earnings_date(ticker))
         if date not in dct:
-            dct[date] = [ticker]
-        else:
-            dct[date].append(ticker)
+            dct[date] = {"Before market open":[],"After market close": [],"An error has occurred; the time coincides with operation hours": []}
+        dct[date][classification].append(ticker)
     return dct
 
 def transform(sorted_earnings_list):
